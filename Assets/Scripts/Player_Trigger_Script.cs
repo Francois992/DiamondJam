511881d89e842d05992c5fc493fc517fba326922
Maxence <mxcolas302@gmail.com>
2020-09-21 17:06:30 +0200

    Ouverture Porte

---
+++ Assets/Scripts/Player_Trigger_Script.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Player_Trigger_Script : MonoBehaviour
+{
+
+    Oxygene_Script oxygene_Script;
+    Porte_Script porte_Script;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        oxygene_Script = FindObjectOfType<Oxygene_Script>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.CompareTag("Porte")) porte_Script.Ouverte = true;
+
+        if (other.tag == "Oxygene")
+        {
+            if (oxygene_Script.Oxygene) UnityEngine.Debug.Log("Vivant");
+            else UnityEngine.Debug.Log("Mort");
+        }
+    }
+
+}
--- Assets/Scripts/Porte_Script.cs
+++ Assets/Scripts/Porte_Script.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Porte_Script : MonoBehaviour
 {
@@ -16,9 +17,13 @@ public class Porte_Script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(!Ouverte)
-        {
-            
-        }
+        if(Ouverte) transform.DOMoveY(70f, 0.5f);
+        else transform.DOMoveY(20f, 0.5f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        UnityEngine.Debug.Log("Porte");
+        if (other.CompareTag("Player")) Ouverte = true;
     }
 }
