c90f0d3ca3ca6c1e98990c22b0300a896d22d7fa
Elise_NGUYEN <elise.nguyen@edu.devinci.fr>
2020-09-21 16:49:33 +0200

    Ajout de l'asset NavMesh et du mouvement pnj

--- Assets/Scripts/Pnj.cs
+++ Assets/Scripts/Pnj.cs
@@ -7,30 +7,71 @@ public class Pnj : MonoBehaviour
     
     public Vector3 position;
     public float timeDead = 2f;
-    public float timer = 0f;
+    public float timerD = 0f;
 
+    public Vector3 posDep;
+    public Vector3 posArr;
+
+    public float timeMove = 1.5f;
+    public float timerM = 0f;
+    public bool move = true;
+    public bool right = true;
+
+    public bool dead = false;
 
     // Start is called before the first frame update
     void Start()
     {
         position = transform.position;
 
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        Death();
+        Move();
+        //Death();
+    }
+
+
+    public void Move()
+    {
+        if (move)
+        {
+            if (right)
+            {
+                transform.Translate(Vector3.right * Time.deltaTime);
+                if (timerM >= timeMove)
+                {
+                   right = false;
+                   timerM = 0;
+                }
+            }
+            else
+            {
+                transform.Translate(Vector3.left * Time.deltaTime);
+                if (timerM >= timeMove)
+                {
+                    right = true;
+                    timerM = 0;
+                }
+            }
+            timerM += Time.deltaTime;
+            
+        }
     }
 
     public void Death()
     {
-        timer += Time.deltaTime;
-        if(timer >= timeDead)
+        move = false;
+        dead = true;
+        timerD += Time.deltaTime;
+        if(timerD >= timeDead)
         {
             this.gameObject.SetActive(false);
         }
     }
+    
+    
 
 }
