02df899623566f2a9dd57715016c44230bfaabf2
Elise_NGUYEN <elise.nguyen@edu.devinci.fr>
2020-09-21 16:19:58 +0200

    Ajout scrip pnj
    Le pnj meut au bout de 2sec si la fonction Death est appelée

---
+++ Assets/Scripts/Pnj.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pnj : MonoBehaviour
+{
+    
+    public Vector3 position;
+    public float timeDead = 2f;
+    public float timer = 0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = transform.position;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Death();
+    }
+
+    public void Death()
+    {
+        timer += Time.deltaTime;
+        if(timer >= timeDead)
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+
+}
