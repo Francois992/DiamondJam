fd50828554b53f78e52881e9d8ac3545e675a318
Elise_NGUYEN <elise.nguyen@edu.devinci.fr>
2020-09-22 09:41:26 +0200

    Zone dangereuse autour du pnj ajouté
    Zone en demi-sphere autour du pnj

--- Assets/Scripts/Pnj_Trigger.cs
+++ Assets/Scripts/Pnj_Trigger.cs
@@ -6,18 +6,21 @@ public class Pnj_Trigger : MonoBehaviour
 {
     public Pnj pnj;
     public SphereCollider sphere;
+    public Vector3 scale;
     
 
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = pnj.transform.position;
+        transform.position = new Vector3(pnj.transform.position.x, 0, pnj.transform.position.z);
+        sphere = GetComponent<SphereCollider>();
+        this.transform.localScale = scale;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = pnj.transform.position;
+        transform.position = new Vector3(pnj.transform.position.x, 0, pnj.transform.position.z);
     }
 
     
