e3d88d8711cee52b4c4cef67c8026852fcbdca4b
Elise_NGUYEN <elise.nguyen@edu.devinci.fr>
2020-09-23 11:56:34 +0200

    Laser ajouté (non létal pour le moment)

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    public bool activation = false;
+    private LineRenderer lr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (activation)
+        {
+            lr.enabled = true;
+            lr.SetPosition(0, transform.position);
+            RaycastHit hit;
+                if (Physics.Raycast(transform.position, transform.forward, out hit))
+                {
+                    if (hit.collider)
+                    {
+                       lr.SetPosition(1, hit.point);
+                    }
+                }
+              else lr.SetPosition(1, transform.position + (transform.forward * 5000));
+        }
+        else
+        {
+            lr.enabled = false;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pnj"))
+        {
+            other.transform.parent.GetComponent<Pnj>().Death();
+        }
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            transform.parent.GetComponent<Player>().enVie = false;
+        }
+    }
+}
+
