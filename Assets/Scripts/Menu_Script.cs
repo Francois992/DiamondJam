5ff96f966460bbb80a1d323389944d05958e9a51
Pavarino <selena.pavarino@devinci.fr>
2020-09-24 13:25:09 +0200

    no message

--- Assets/Scripts/Menu_Script.cs
+++ Assets/Scripts/Menu_Script.cs
@@ -13,9 +13,13 @@ public class Menu_Script : MonoBehaviour
 
     bool SurMenuPrincipal;
 
+    GameMaster gm;
+
     private void Start()
     {
         SurMenuPrincipal = true;
+
+        gm = FindObjectOfType<GameMaster>();
     }
 
     public void QuitterLeJeu()
@@ -45,5 +49,22 @@ public class Menu_Script : MonoBehaviour
         SurMenuPrincipal = !SurMenuPrincipal;
         MenuPrincipal.DOLocalMoveX(SurMenuPrincipal == true ? 0f : -1101f, 0.5f);
         MenuCredits.DOLocalMoveX(SurMenuPrincipal == false ? 0f : 1101f, 0.5f);
+    }
+
+    public void DefaiteRejouer()
+    {
+        foreach (GameObject player in gameObject.GetComponent<GameManager>().Players)
+        {
+            player.transform.localPosition = new Vector3(gm.lastPositionCheckpoint.x + (player.GetComponent<Player>().playerId == 0 ? 2 : -2), gm.lastPositionCheckpoint.y, gm.lastPositionCheckpoint.z);
+            StartCoroutine(WaitToRespawn());
+        }
+    }
+
+    IEnumerator WaitToRespawn()
+    {
+        yield return new WaitForSeconds(0.5f);
+        for(int i = 0; i < gameObject.GetComponent<GameManager>().Players.Length; i++) gameObject.GetComponent<GameManager>().Players[i].GetComponent<Player>().respawnPlayer = true;
+        yield return new WaitForSeconds(0.5f);
+        gameObject.GetComponent<GameManager>().menuDefaite.SetActive(false);
     }
 }
