006172a7cab04601c88b9ded4f8719ec4b458225
Eloi FG <eloi-fg@gmx.com>
2020-09-24 21:07:13 +0200

    updated level1

---
+++ Assets/falling.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class falling : MonoBehaviour
+{
+    public AudioSource source;
+    public AudioClip fallingZic;
+
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+            {
+            source.PlayOneShot(fallingZic);
+        }
+    }
+}
