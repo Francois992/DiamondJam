e6c3885c968f9fb84627f37395eb9ce9b6fb84a6
Elise_NGUYEN <elise.nguyen@edu.devinci.fr>
2020-09-25 11:46:46 +0200

    Ajout matérial laser et zone du pnj update

--- Assets/Scripts/Pnj_Trigger.cs
+++ Assets/Scripts/Pnj_Trigger.cs
@@ -7,13 +7,14 @@ public class Pnj_Trigger : MonoBehaviour
     public Pnj pnj;                 //Choix du pnj 
     public SphereCollider sphere;   //Collider désigné
     public Vector3 scale;           //Taille de la zone dangereuse
+    
 
     public Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(pnj.transform.position.x, pnj.transform.position.y, pnj.transform.position.z);
+        transform.position = new Vector3(pnj.transform.position.x, pnj.transform.position.y-0.97f, pnj.transform.position.z);
         sphere = GetComponent<SphereCollider>();
         this.transform.localScale = scale;
     }
@@ -27,7 +28,7 @@ public class Pnj_Trigger : MonoBehaviour
 
     public void Follow()
     {
-        transform.position = new Vector3(pnj.transform.position.x, pnj.transform.position.y, pnj.transform.position.z);
+        transform.position = new Vector3(pnj.transform.position.x, pnj.transform.position.y -0.97f, pnj.transform.position.z);
     }
 
     private void OnTriggerEnter(Collider other)
